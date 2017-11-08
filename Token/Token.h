59726b7bdec89f10a6a7461bb02d5ec8827f3b2b
Aaron Wright <aaron.c.wright@icloud.com>
2017-11-08 16:22:17 -0600

    Initial Commit

---
+++ Token/Token.h
@@ -0,0 +1,19 @@
+//
+//  Token.h
+//  Token
+//
+//  Created by Aaron Wright on 11/8/17.
+//  Copyright © 2017 Aaron Wright. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Token.
+FOUNDATION_EXPORT double TokenVersionNumber;
+
+//! Project version string for Token.
+FOUNDATION_EXPORT const unsigned char TokenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Token/PublicHeader.h>
+
+
