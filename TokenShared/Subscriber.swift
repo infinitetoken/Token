122973abfd729996b95c732e04c04ff57159a554
pfoster <paulfostergameroom@gmail.com>
2018-01-03 15:27:29 -0600

    bug fix

--- TokenShared/Subscriber.swift
+++ TokenShared/Subscriber.swift
@@ -15,6 +15,11 @@ public protocol Subscriber: class {
 
 public extension Subscriber {
     
+    public var token: Token { return Token.shared }
+    
+    public func dispatch(_ action: Action) {Token.shared.dispatch(action: action)}
+    public func dispatch(_ actionCreator: ActionCreator) {Token.shared.dispatch(actionCreator: actionCreator)}
+    
     public func subscribe() { token.subscribe(subscriber: self) }
     public func unsubscribe() { token.unsubscribe(subscriber: self) }
     
